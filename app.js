d9468513a8c723af26101a7ced0262951822784e
Harry Evans <henryjohnevans@icloud.com>
2021-09-03 09:27:44 +0100

    heroku setup

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors' //* <-- This is new
+
+// import { connectDb } from './db/helpers.js'
+import logger from './middleware/logger.js'
+import router from './views/router.js'
+import errorHandler from './middleware/errorHandler.js'
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors())
+
+app.use(logger)
+
+app.use('/api', router)
+
+app.use(errorHandler)
+
+export default app
